65c045c65fad243b860fce6f571be9cbbea31fc0
Iktisad Rashid <iktisad.rashid@hotmail.com>
2021-07-08 15:09:57 +0600

    first commit express js setup

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+// import usersRoutes from './routes/users.js';
+// import resourceRoutes from './routes/resources.js';
+// import appointmentRoutes from './routes/appointments.js';
+import mongoose from 'mongoose';
+import {} from 'dotenv/config';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// route middleware
+// app.use('/users', usersRoutes);
+// app.use('/api/resources', resourceRoutes);
+// app.use('/appointments', appointmentRoutes);
+// routes
+
+app.get('/', (req, res)=>{
+    res.send('Yo I m active');
+    console.log('Yay');
+});
+mongoose.connect(process.env.CONNECTION_STRING.replace('<DBPORT>', process.env.DBPORT),
+{
+    useNewUrlParser:true, 
+    useUnifiedTopology:true,
+    useFindAndModify: false
+}, 
+    () => {
+    console.log("Connected To Patinet Database");
+});
+
+app.listen(process.env.PORT, ()=> console.log(`Running On Port http://localhost:${process.env.PORT}`));
